d96daf495fd05b13ba2aea2ae8e9ed37d925d5d4
wigueta <brianleonardomtzcoboswigueta@gmail.com>
2023-11-29 22:00:11 -0600

    Update cv. Add Card.astro, change icons.jsx to .astro. Update UI.

--- src/components/react/ProgressBar.jsx
+++
@@ -1,8 +0,0 @@
-import { motion, useScroll } from "framer-motion";
-export const ProgressBar = () => {
-  
-  const { scrollYProgress } = useScroll();
-  return (
-    <motion.div className=" fixed bottom-0 left-0 right-0 h-[8px] bg-green-400 origin-[0%]" style={{ scaleX: scrollYProgress }} />
-  )
-}
